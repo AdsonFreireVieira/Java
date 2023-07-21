6ba5ff5c20db598b08689ff107af443949181f66
Adson <Adsonfr.vieira@hotmail.com>
2023-07-20 22:47:15 -0300

    filtros Stream

--- src/streams/Streams.java
+++ src/streams/Streams.java
@@ -10,7 +10,15 @@ public class Streams {
 		List<Integer> asList = Arrays.asList(1,4,6,7,8,2);
 		
 		asList.stream()
-		.filter( e -> e % 2== 0)
-		.forEach( e -> System.out.print(e));
+		.filter( e -> e % 2== 0)  // filtra pares 
+		.forEach( e -> System.out.print(e)); // percorre lista filtrada com numeros pares
+		
+		
+		asList.stream()
+		.limit(3)  // limita 3 primeiros
+		.skip(2)   // ignora os  2 primeiros elementos
+		.forEach( e ->System.out.println(e));
 	}
+	
+
 }
--- src/thread/Threads.java
+++ src/thread/Threads.java
@@ -18,7 +18,7 @@ public class Threads {
 		Thread t1 = new Thread(thread1);
 		Thread t2 = new Thread(thread2);
 		Thread t3 = new Thread(thread3);
-		
+		 
 		t1.start();
 		t2.start();
 		t3.start();
