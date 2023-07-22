ae723f34a252f2ecec8a6fece4fa4f5afe7846f4
Adson <Adsonfr.vieira@hotmail.com>
2023-07-21 23:54:04 -0300

    streams

--- src/streams/Streams.java
+++ src/streams/Streams.java
@@ -1,7 +1,9 @@
 package streams;
 
+import java.nio.file.spi.FileSystemProvider;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Streams {
 	
@@ -9,16 +11,36 @@ public class Streams {
 		
 		List<Integer> asList = Arrays.asList(1,4,6,7,8,2);
 		
+		String[] strings = {"red","Orange","yellow","green","blue",""};
+		
+		 System.out.println("======================ORIGINAL===============");
+		System.out.println("Strings Original " + Arrays.asList(strings));
+       
+		System.out.println("===============================================");
+        System.out.println("MAIUSCULA "+
+        		Arrays.stream(strings)
+        		.map(String::toUpperCase)
+        		.collect(Collectors.toList()));
+        
+		
+		
+		
 		asList.stream()
 		.filter( e -> e % 2== 0)  // filtra pares 
 		.forEach( e -> System.out.print(e)); // percorre lista filtrada com numeros pares
 		
 		
 		asList.stream()
+		 // filtros Stream
 		.limit(3)  // limita 3 primeiros
 		.skip(2)   // ignora os  2 primeiros elementos
+		.limit(4)  // pega os 4  primeiros
+		.distinct() // nao permite que processe elementos repetidos 
+		.map( e -> e *2) // recebe funcoes  nao altera lista original sltera lista se passsar pelo filtro 	
 		.forEach( e ->System.out.println(e));
 	}
+	
+	
 	
 
 }
