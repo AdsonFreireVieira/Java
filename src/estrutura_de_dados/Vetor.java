468c6f87062fc57ccde23eedba391132398570c6
Adson <Adsonfr.vieira@hotmail.com>
2023-08-17 21:45:31 -0300

    vetor

--- src/estrutura_de_dados/Vetor.java
+++ src/estrutura_de_dados/Vetor.java
@@ -4,6 +4,27 @@ public class Vetor {
 
 	public static void main(String []args) {
 		
-		//
+		String palavra [] = {"pera","uva","Banana","maca"};
+		
+		int numeros [] = {6,23,63,8,9,1,8,72};
+		
+		
+		 System.out.println("=================Strings==========================");
+		for (String st: palavra) {
+		
+			System.out.println(st.toUpperCase());
+			
+		
+			
+		}
+		 
+		System.out.println(palavra.length);
+		System.out.println(palavra.hashCode());
+		System.out.println("=====================================================");
+		
+		
+		System.out.println("=================================int==============");
+		
+		
 	}
 }
