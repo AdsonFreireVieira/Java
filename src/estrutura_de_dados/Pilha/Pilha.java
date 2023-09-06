ca702551dd877459caf766ae2a16f3098ddb28fd
Adson <Adsonfr.vieira@hotmail.com>
2023-09-05 22:49:58 -0300

    teste pilha

--- src/estrutura_de_dados/Pilha/Pilha.java
+++ src/estrutura_de_dados/Pilha/Pilha.java
@@ -9,10 +9,19 @@ public class Pilha {
 		
 		ArrayStack<Integer> A = new ArrayStack<Integer>();
 		
-		A.status(" new ArrayStack<Integer> A " , null);
-		A.push(8);
-		A.status("A.push(7) " , null);
-	
+		A.status("new Array <Integer> A"  , null);
+		A.push(7);
+		A.status("A.push(7)", null);
+	    A.push(2);
+	    A.push(3);
+	    A.status(" nullll", A);
+	    A.pop();
+ 	    A.status("", A);
+        o=A.top();
+        A.status(null, o);
+        A.pop();
+        A.status(null, A);
+      
 	}
 
 }
