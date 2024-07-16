385172367ed50d76cc882ce9324d767cfa1136eb
Adson <adsonfr.vieira@hotmail.com>
2024-07-15 21:38:21 -0300

    add: class aninhada / class static

---
+++ java11/src/enums/OnlyONE.java
@@ -0,0 +1,10 @@
+package enums;
+
+public enum OnlyONE {
+	ONCE(true);
+
+	private OnlyONE(boolean b) {
+		System.out.println("Construindo");
+	}
+
+}
--- java11/src/enums/Principal.java
+++ java11/src/enums/Principal.java
@@ -1,44 +1,49 @@
 package enums;
 
 public class Principal {
-	
-	public static void main(String []args) {
-	Season s =  Season.SUMMER;
-	
-	//System.out.println(Season.SUMMER);
-	
-	//System.out.println(s == Season.SUMMER);
-	
-	
-      for(Season season : Season.values()) {
-		
-      //System.out.println(season.name() + "  "+ season.ordinal());
-      
-      //System.out.println(season.valueOf("SUMMER"));
-	}
-      
-      Season summer = Season.SUMMER;
-      
-      switch (summer){
-	case  WINTER: {
-		
-		System.out.println("Winter");
-		break;
-	}
-	case  SUMMER:{
-		System.out.println("Summer is");
-		break;
-	}
 
-	default:
-		System.out.println("ERRO");
-	}
-      
+	public static void main(String[] args) {
+		Season s = Season.SUMMER;
+
+		// System.out.println(Season.SUMMER);
+
+		// System.out.println(s == Season.SUMMER);
+
+		for (Season season : Season.values()) {
+
+			// System.out.println(season.name() + " "+ season.ordinal());
+
+			// System.out.println(season.valueOf("SUMMER"));
+		}
+
+		Season summer = Season.SUMMER;
+
+		switch (summer) {
+		case WINTER: {
+
+			System.out.println("Winter");
+			break;
+		}
+		case SUMMER: {
+			System.out.println("Summer is");
+			break;
+		}
+
+		default:
+			System.out.println("ERRO");
+		}
+
+		switch (summer) {
+
+		}
+
+		System.out.println("begin");
+
+		OnlyONE firstCall = OnlyONE.ONCE;
+
+		OnlyONE secondCall = OnlyONE.ONCE;
+
+		System.out.println("End");
+
 	}
 }
-
-
-
-
-
-
